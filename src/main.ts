6d233002fdbcebc20ef82a336bdbeacd70122017
Syldel <nightfand@hotmail.com>
2026-02-27 15:08:30 +0100

    feat(api): configure CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const origins = configService
+    .get<string>('CORS_ORIGINS')
+    ?.split(',')
+    .map((origin) => origin.trim());
+
+  const corsOptions: CorsOptions = {
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin) return callback(null, true);
+
+      if (origins?.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(null, false);
+    },
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
 
   // C'est cette ligne qui active la magie de class-validator
   app.useGlobalPipes(
@@ -16,4 +43,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+});
